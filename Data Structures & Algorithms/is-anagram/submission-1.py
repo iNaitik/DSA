bf2daf6f108fc5259e4c9d346a87b2173bbf0436
Naitik Seth <sethnaitikgg77@gmail.com>
2026-06-06 20:23:49 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashset1 = {}
+        hashset2 = {}
+        for i in s:
+            if i in hashset1:
+                hashset1[i] += 1
+            else:
+                hashset1[i] = 1
+        for i in t:
+            if i in hashset2:
+                hashset2[i] += 1
+            else:
+                hashset2[i] = 1
+
+        if hashset1 == hashset2:
+            return True
+        else:
+           return False
